a45b03a88b5b46d06f16e48fa926b77698eeb45b
Soya Sano <soyasano2180plus1@gmail.com>
2019-08-09 20:38:26 +0900

    Setup api class and Create Implement Clss

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Queue<Object> queue = new QueueImpl();
+        System.out.println(queue.isEmpty());
+    }
+}
---
+++ src/QueueImpl.java
@@ -0,0 +1,22 @@
+
+class QueueImpl implements Queue<Object> {
+    @Override
+    public Queue<Object> deQueue() {
+        return null;
+    }
+
+    @Override
+    public Object head() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return true;
+    }
+
+    @Override
+    public Queue<Object> enQueue(Object obj) {
+        return null;
+    }
+}
